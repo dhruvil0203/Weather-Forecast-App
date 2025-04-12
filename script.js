303b7da70226fae200ee25259940d387600ec83a
Dhruvil Mistry <dkmistry0203@gmail.com>
2025-04-12 13:00:12 +0530

    Add Favicon Icon

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const apikey = "7152c35def31e7bbd1f5a7f12512214d";
-const apiurl =
-  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
@@ -19,8 +18,7 @@ async function checkWeather(city) {
     document.querySelector(".city").innerText = data.name;
     document.querySelector(".temprature").innerText =
       Math.round(data.main.temp) + "°c";
-    document.querySelector(".humidity").innerText =
-      data.main.humidity + "%";
+    document.querySelector(".humidity").innerText = data.main.humidity + "%";
     document.querySelector(".wind").innerText = data.wind.speed + " Km/h";
 
     const weatherCondition = data.weather[0].main.toLowerCase();
